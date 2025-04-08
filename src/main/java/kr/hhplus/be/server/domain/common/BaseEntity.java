6db68511b542f06e47d02803e9cda3a6adfed070
klettermi <mi_aa_@naver.com>
2025-04-08 21:23:16 +0900

    [FEAT] BaseEntity 추가

---
+++ src/main/java/kr/hhplus/be/server/domain/common/BaseEntity.java
@@ -0,0 +1,35 @@
+package kr.hhplus.be.server.domain.common;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
