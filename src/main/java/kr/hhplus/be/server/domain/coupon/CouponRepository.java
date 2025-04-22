7d205f784fa712ac81e8577fa91e974efac8ceb6
klettermi <mi_aa_@naver.com>
2025-04-22 09:56:17 +0900

    [FEAT] 쿠폰 비관적 락 적용

--- src/main/java/kr/hhplus/be/server/domain/coupon/CouponRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/coupon/CouponRepository.java
@@ -1,11 +1,16 @@
 package kr.hhplus.be.server.domain.coupon;
 
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 public interface CouponRepository {
+
     Optional<Coupon> findByCouponCode(String couponCode);
 
     Coupon save(Coupon coupon);
 
     void deleteAll();
+
+    Optional<Coupon> findByCouponCodeForUpdate(@Param("code") String couponCode);
 }
