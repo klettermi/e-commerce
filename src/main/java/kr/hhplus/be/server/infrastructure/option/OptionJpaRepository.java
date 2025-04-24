70e294cbcfb47a5cc2e356ff78249b90575ab0d4
klettermi <mi_aa_@naver.com>
2025-04-24 17:16:02 +0900

    [FIX] Option 수정

--- src/main/java/kr/hhplus/be/server/infrastructure/option/OptionJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/option/OptionJpaRepository.java
@@ -1,6 +1,5 @@
 package kr.hhplus.be.server.infrastructure.option;
 
-import kr.hhplus.be.server.domain.common.BaseEntity;
 import kr.hhplus.be.server.domain.option.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 
