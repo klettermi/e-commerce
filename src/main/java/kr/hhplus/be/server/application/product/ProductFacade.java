58d224aa96792facf5f520cfa4f901571d65719a
klettermi <mi_aa_@naver.com>
2025-05-08 23:51:58 +0900

    [FEAT] Cache 적용

--- src/main/java/kr/hhplus/be/server/application/product/ProductFacade.java
+++ src/main/java/kr/hhplus/be/server/application/product/ProductFacade.java
@@ -4,6 +4,8 @@ import kr.hhplus.be.server.domain.product.ProductCommand;
 import kr.hhplus.be.server.domain.product.ProductInfo;
 import kr.hhplus.be.server.domain.product.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,9 @@ import java.util.stream.Collectors;
 public class ProductFacade {
     private final ProductService productService;
 
+    /**
+     * 페이징된 상품 목록 조회
+     */
     public ProductOutput.Page getProductList(ProductInput.List input) {
         var pageable = PageRequest.of(input.getPage(), input.getSize());
         ProductInfo.ProductPage info = productService.getProductList(
@@ -35,6 +40,10 @@ public class ProductFacade {
                 .build();
     }
 
+    /**
+     * 최근 3일간 최다 판매 상품 조회 (Top N) - 캐시 적용
+     */
+    @Cacheable(value = "topSellingLast3Days", key = "#input.topN")
     public ProductOutput.TopSellingList getTopSellingProductsLast3Days(ProductInput.TopSelling input) {
         ProductInfo.TopSellingList info = productService.getTopSellingProductsLast3Days(
                 ProductCommand.GetTopSelling.of(input.getTopN())
@@ -51,5 +60,12 @@ public class ProductFacade {
                 .products(items)
                 .build();
     }
-}
 
+    /**
+     * 최근 3일간 최다 판매 상품 캐시 무효화
+     */
+    @CacheEvict(value = "topSellingLast3Days", allEntries = true)
+    public void evictTopSellingLast3DaysCache() {
+        // 빈 메서드로 캐시 무효화 처리
+    }
+}
